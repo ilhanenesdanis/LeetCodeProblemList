dc4ec830175d6ea814cf1868fafd713b8418c819
ilhan enes daniş <danisenes3@gmail.com>
2024-10-24 15:29:23 +0300

    PalindromeNumber

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿static bool IsPalindrome(int x)
+{
+    int originalNumber = x;
+    int reversedNumber = 0;
+    while (x > 0)
+    {
+        int remainder = x % 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+        x /= 10;
+    }
+    return originalNumber == reversedNumber;
+}
+
+
+
+var asd = IsPalindrome(12321);
+
+Console.ReadLine();
