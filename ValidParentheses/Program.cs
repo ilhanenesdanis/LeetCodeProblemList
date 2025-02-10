fc8024e911e21500d9b3886207b21d6c5f590531
İlhan Enes Daniş <danisenes3@gmail.com>
2025-02-10 23:08:05 +0300

    valid parentheses

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections;
+
+Solution solution = new Solution();
+
+Console.WriteLine(solution.IsValid("([])"));
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        if (s.Any(c => !"(){}[]".Contains(c)))
+        {
+            return false;
+        }
+       
+        var inputText = "";
+        foreach (var c in s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                inputText += c;
+            }
+            else
+            {
+                if (inputText.Length == 0)
+                {
+                    return false;
+                }
+
+                char last = inputText[^1];
+
+
+                if ((c == ')' && last == '(') ||
+                (c == '}' && last == '{') ||
+                (c == ']' && last == '['))
+                {
+                    inputText = inputText.Remove(inputText.Length - 1); 
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return inputText.Length == 0;
+    }
+}
