65e4784c87218d5d0ebeb800c6e975e78a034341
İlhan Enes Daniş <danisenes3@gmail.com>
2025-02-20 10:47:19 +0300

    RemoveElement

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+
+Solution solution = new Solution();
+
+int[] nums = [3, 2, 2, 3];
+
+Console.WriteLine(solution.RemoveElement(nums, 3));
+Console.WriteLine(solution.RemoveElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+
+
+
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        if (nums.Length == 0)
+            return 0;
+
+        var currentNums = nums.ToList();
+
+        currentNums.RemoveAll(x => x == val);
+
+        currentNums.CopyTo(nums);
+
+        return currentNums.Count;
+    }
+}
